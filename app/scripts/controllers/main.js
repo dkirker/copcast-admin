69f08492944339a18878147969ee3aba40b70225
Alex Salgado <alexsalgado1@gmail.com>
2015-02-03 14:47:46 -0200

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name copcastAdminApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the copcastAdminApp
+ */
+angular.module('copcastAdminApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
