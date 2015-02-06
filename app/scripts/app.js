9f3592b3950b46b6c2038121e57bdc7b9ad4bb2a
brunosiqueira <Breakdown14>
2015-02-06 16:15:58 -0200

    creating login screen

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -32,7 +32,29 @@ angular
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
+      .when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'LoginCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
+  }).factory('authInterceptor', function ($rootScope, $q, $window) {
+    return {
+      request: function (config) {
+        config.headers = config.headers || {};
+        if ($window.sessionStorage.token) {
+          config.headers.Authorization = 'Bearer ' + $window.sessionStorage.token;
+        }
+        return config;
+      },
+      response: function (response) {
+        if (response.status === 401) {
+          // handle the case where the user is not authenticated
+        }
+        return response || $q.when(response);
+      }
+    };
+  }).config(function ($httpProvider) {
+    $httpProvider.interceptors.push('authInterceptor');
   });
---
+++ app/scripts/controllers/login.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name copcastAdminApp.controller:LoginCtrl
+ * @description
+ * # LoginCtrl
+ * Controller of the copcastAdminApp
+ */
+angular.module('copcastAdminApp')
+  .controller('LoginCtrl', function ($scope) {
+    $scope.user = {username: 'john.doe', password: 'foobar'};
+    $scope.message = '';
+    $scope.submit = function () {
+      $http
+        .post('/authenticate', $scope.user)
+        .success(function (data, status, headers, config) {
+          $window.sessionStorage.token = data.token;
+          $scope.message = 'Welcome';
+        })
+        .error(function (data, status, headers, config) {
+          // Erase the token if the user fails to log in
+          delete $window.sessionStorage.token;
+
+          // Handle login errors here
+          $scope.message = 'Error: Invalid user or password';
+        });
+    };
+  });
