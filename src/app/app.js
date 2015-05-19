e1ecd3092d3a8423835d8fba4244f30d9523ddec
Leonardo Nicolas <leonicolas@gmail.com>
2015-05-18 23:01:40 -0300

    Update build to support SASS, GULP and Angular 1.3

---
+++ src/app/app.js
@@ -0,0 +1,93 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name copcastAdminApp
+ * @description
+ * # copcastAdminApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('copcastAdminApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'ui.map',
+    'ui.bootstrap',
+    'http-auth-interceptor',
+    'angular-jwplayer' ,
+    'toaster',
+    'ngFileUpload'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/views/realtime.html',
+        controller: 'RealtimeCtrl',
+      })
+      .when('/main', {
+        templateUrl: 'app/views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/logout', {
+        controller: 'LogoutCtrl',
+        template: ''
+      })
+      .when('/history', {
+        templateUrl: 'app/views/history.html',
+        controller: 'HistoryCtrl'
+      })
+
+      .when('/group-list', {
+        templateUrl: 'app/views/groups/group-list.html',
+        controller: 'GroupsListCtrl'
+      })
+      .when('/group-detail/:id', {
+        templateUrl: 'app/views/groups/group-detail.html',
+        controller: 'GroupsDetailsCtrl'
+      })
+      .when('/group-creation', {
+        templateUrl: 'app/views/groups/group-creation.html',
+        controller: 'GroupsCreationCtrl'
+      })
+      .when('/group-destroy/:id', {
+        templateUrl: 'app/views/groups/group-destroy.html',
+        controller: 'GroupsDestroyCtrl'
+      })
+      .when('/user-list', {
+        templateUrl: 'app/views/users/user-list.html',
+        controller: 'UsersListCtrl'
+      })
+      .when('/user-detail/:id', {
+        templateUrl: 'app/views/users/user-detail.html',
+        controller: 'UsersDetailsCtrl'
+      })
+      .when('/user-creation', {
+        templateUrl: 'app/views/users/user-creation.html',
+        controller: 'UsersCreationCtrl'
+      })
+      .when('/user-destroy/:id', {
+        templateUrl: 'app/views/users/user-destroy.html',
+        controller: 'UsersDestroyCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }).run(function($rootScope, $location, loginService) {
+
+    $rootScope.$on('event:auth-loginRequired', function() {
+      loginService.show();
+    });
+
+    $rootScope.isActive = function (viewLocation) {
+      return viewLocation === $location.path();
+    };
+  });
+
+//insert the constant value ServerUrl
+//angular.module('copcastAdminApp').constant('ServerUrl', 'http://mogi-api.igarape.org');
+angular.module('copcastAdminApp').constant('ServerUrl', 'http://localhost:3000');
---
+++ src/app/directives/username.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name copcastAdminApp.directive:userName
+ * @description
+ * # userName
+ */
+angular.module('copcastAdminApp')
+  .directive('userName', function () {
+    return {
+      templateUrl: "app/views/userName.html",
+      restrict: 'E'
+
+    };
+  });
