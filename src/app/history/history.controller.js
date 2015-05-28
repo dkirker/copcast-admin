81edf5bdd53d81143f7e0a203526ee8137994b05
Leonardo Nicolas <leonicolas@gmail.com>
2015-05-28 09:11:12 -0300

    Add user dropdown directive to history page with server user list

--- src/app/history/history.controller.js
+++ src/app/history/history.controller.js
@@ -3,9 +3,24 @@
 
   var app = angular.module('copcastAdminApp');
 
-  app.controller('HistoryCtrl', function ($scope) {
+  app.controller('HistoryCtrl', function ($scope, userService) {
+
+    loadUsers();
+
+    $scope.userChanged = function userChanged(user) {
+      console.log('user: ', user);
+    };
+
+    function loadUsers() {
+      userService
+        .listUsers()
+        .then(function(users) {
+          $scope.users = users;
+        })
+        .catch(function(data, status) {
+          console.log('error', data, status);
+        });
+    }
   });
 
-    };
-  });
 })(window.angular);
