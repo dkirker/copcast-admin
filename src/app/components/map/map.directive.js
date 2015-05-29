f542f82daa920bd1b6fdc10fa9a0fb8cb86926ec
Leonardo Nicolas <leonicolas@gmail.com>
2015-05-29 07:51:10 -0300

    Add position property to map directive

--- src/app/components/map/map.directive.js
+++ src/app/components/map/map.directive.js
@@ -6,12 +6,23 @@
   app.directive('map', function() {
     return {
       restrict: 'EA',
+      scope: {
+        position: '='
+      },
       link: function(scope, element, attrs) {
         var mapOptions = {
-          zoom: 8,
-          center: new google.maps.LatLng(-34.397, 150.644)
+          zoom: 3,
+          center: new google.maps.LatLng(0, 0)
         };
         var map = new google.maps.Map(element[0], mapOptions);
+        scope.$watch('position', function() {
+          if(scope.position) {
+            if(scope.position.lat && scope.position.lng) {
+              map.setZoom(15);
+              map.panTo(angular.copy(scope.position));
+            }
+          }
+        }, true);
       }
     };
   });
