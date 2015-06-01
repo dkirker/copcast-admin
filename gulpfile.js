4906d23af048a37ff1d88c884817305a4771153f
brunosiqueira <bsiqueira@gmail.com>
2015-06-01 20:06:13 +0100

    correcting travis file

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var wrench = require('wrench');
+var karma = require('karma').server;
 
 var options = {
   src: 'src',
@@ -25,6 +26,13 @@ wrench.readdirSyncRecursive('./gulp').filter(function(file) {
   return (/\.(js|coffee)$/i).test(file);
 }).map(function(file) {
   require('./gulp/' + file)(options);
+});
+
+gulp.task('tests', function(done) {
+  return karma.start({
+    configFile: 'karma.conf.js',
+    singleRun: true
+  }, done);
 });
 
 gulp.task('default', ['clean'], function () {
