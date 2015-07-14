1c4fdf963d8d5426599b4f9954be338fa3fb7025
Leonardo Nicolas <leonicolas@gmail.com>
2015-07-14 13:28:57 -0300

    Add user videos by period service

--- src/app/services/user.service.js
+++ src/app/services/user.service.js
@@ -13,128 +13,133 @@
    * # Template for user CRUD
    * Factory in the copcastAdminApp.
    */
-  angular.module('copcastAdminApp')
-    .factory('userService', function($compile, $templateCache, $q, $http, ServerUrl) {
-      var service = {};
+  var app = angular.module('copcastAdminApp');
 
-      service.delete = function deleteUser( userId, userName ) {
-        if (infoWindow) {
-          infoWindow.close();
-        }
-        infoWindow = new google.maps.InfoWindow({
-          content: $compile($templateCache.get('balloon.html'))(scope)[0]
-        });
-        infoWindow.open(scope.myMap,scope.currentUser.marker);
-      };
+  app.factory('userService', function($compile, $templateCache, $q, $http, ServerUrl) {
+    var service = {};
 
-      service.listUsers = function listUsers() {
-        var defer = $q.defer();
-        $http
-          .get(ServerUrl + '/users')
-          .success(function(data) {
-            angular.forEach(data, function(user) {
-              user.profilePicture = generateProfilePictureAddress(ServerUrl, user);
-            });
-            defer.resolve(data);
-          })
-          .error(function(data, status) {
-            defer.reject(data, status);
-          });
-        return defer.promise;
-      };
-
-      service.getUser = function getUser(userId) {
-        var defer = $q.defer();
-        $http
-          .get(ServerUrl + '/users/' + userId)
-          .success(function(user) {
-            user.profilePicture = generateProfilePictureAddress(ServerUrl, user);
-            defer.resolve(user);
-          })
-          .error(function(data, status) {
-            defer.reject(data, status);
-          });
-        return defer.promise;
+    service.delete = function deleteUser( userId, userName ) {
+      if (infoWindow) {
+        infoWindow.close();
       }
+      infoWindow = new google.maps.InfoWindow({
+        content: $compile($templateCache.get('balloon.html'))(scope)[0]
+      });
+      infoWindow.open(scope.myMap,scope.currentUser.marker);
+    };
 
-      service.getUserLocations = function getUserLocations(userId, fromDate, toDate, accuracy) {
-        var defer = $q.defer();
-
-        fromDate = moment(fromDate).format('YYYY-MM-DD');
-        toDate = toDate ? '/' + moment(toDate).format('YYYY-MM-DD') : '';
-        accuracy = accuracy ? '/' + accuracy : '';
-
-        var endPoint = ServerUrl + '/users/' + userId + '/locations/' + fromDate + toDate + accuracy;
-        $http
-          .get(endPoint)
-          .success(function(data) {
-            console.log('service.getUserLocations', data);
-            defer.resolve(data);
-          })
-          .error(function(data, status) {
-            defer.reject(data, status);
+    service.listUsers = function listUsers() {
+      var defer = $q.defer();
+      $http
+        .get(ServerUrl + '/users')
+        .success(function(data) {
+          angular.forEach(data, function(user) {
+            user.profilePicture = generateProfilePictureAddress(ServerUrl, user);
           });
-        return defer.promise;
-      };
-
-      service.getUserVideos = function getUserVideos(userId, date) {
-        var defer = $q.defer();
-        var dateFmt = moment(date).format('YYYY-MM-DD');
-        $http
-          .get(ServerUrl + '/users/' + userId + '/videos/from/' + dateFmt )
-          .success(function(data) {
-            angular.forEach(data, function(video) {
-              video.src=ServerUrl + '/users/' + userId + '/videos/' + video.id + '.mp4';
-            });
-            defer.resolve(data);
-          })
-          .error(function(data, status) {
-            defer.reject(data, status);
-          });
-        return defer.promise;
-      };
-
-      service.getMyData = function getMyData(){
-        var defer = $q.defer();
-        $http.get(ServerUrl + '/users/me').success(function(data) {
           defer.resolve(data);
         })
         .error(function(data, status) {
           defer.reject(data, status);
         });
-        return defer.promise;
-      };
+      return defer.promise;
+    };
 
-      service.getOnlineUsers = function getOnlineUsers(){
-        var defer = $q.defer();
-        $http.get(ServerUrl + '/users/online')
-          .success(function(data) {
-            defer.resolve(data);
-          })
-          .error(function(data, status) {
-            defer.reject(data, status);
-          });
-        return defer.promise;
-      };
-
-      service.getStreamingUsers = function getStreamingUsers(){
-        var defer = $q.defer();
-        $http.get(ServerUrl + '/users/streaming')
-          .success(function(data) {
-            defer.resolve(data);
-          })
-          .error(function(data, status) {
-            defer.reject(data, status);
-          });
-        return defer.promise;
-      };
-
-      return service;
-    });
-
-    function generateProfilePictureAddress(serverUrl, user) {
-      return user.profilePicture
-        ? serverUrl + '/pictures/' + user.id + '/small/show'
-        : '/assets/images/anonuser.png';
+    service.getUser = function getUser(userId) {
+      var defer = $q.defer();
+      $http
+        .get(ServerUrl + '/users/' + userId)
+        .success(function(user) {
+          user.profilePicture = generateProfilePictureAddress(ServerUrl, user);
+          defer.resolve(user);
+        })
+        .error(function(data, status) {
+          defer.reject(data, status);
+        });
+      return defer.promise;
     }
+
+    service.getUserLocations = function getUserLocations(userId, fromDate, toDate, accuracy) {
+      var defer = $q.defer();
+
+      fromDate = moment(fromDate).format('YYYY-MM-DD');
+      toDate = toDate ? '/' + moment(toDate).format('YYYY-MM-DD') : '';
+      accuracy = accuracy ? '/' + accuracy : '';
+
+      var endPoint = ServerUrl + '/users/' + userId + '/locations/' + fromDate + toDate + accuracy;
+      $http
+        .get(endPoint)
+        .success(function(data) {
+          console.log('service.getUserLocations', data);
+          defer.resolve(data);
+        })
+        .error(function(data, status) {
+          defer.reject(data, status);
+        });
+      return defer.promise;
+    };
+
+    service.getUserVideos = function getUserVideos(userId, fromDate, toDate) {
+      var defer = $q.defer();
+
+      fromDate = moment(fromDate).format('YYYY-MM-DD');
+      toDate = toDate ? '/' + moment(toDate).format('YYYY-MM-DD') : '';
+
+      $http
+        .get(ServerUrl + '/users/' + userId + '/videos/from/' + fromDate + toDate )
+        .success(function(data) {
+          angular.forEach(data, function(video) {
+            video.src=ServerUrl + '/users/' + userId + '/videos/' + video.id + '.mp4';
+            video.userId = userId;
+          });
+          defer.resolve(data);
+        })
+        .error(function(data, status) {
+          defer.reject(data, status);
+        });
+      return defer.promise;
+    };
+
+    service.getMyData = function getMyData(){
+      var defer = $q.defer();
+      $http.get(ServerUrl + '/users/me').success(function(data) {
+        defer.resolve(data);
+      })
+      .error(function(data, status) {
+        defer.reject(data, status);
+      });
+      return defer.promise;
+    };
+
+    service.getOnlineUsers = function getOnlineUsers(){
+      var defer = $q.defer();
+      $http.get(ServerUrl + '/users/online')
+        .success(function(data) {
+          defer.resolve(data);
+        })
+        .error(function(data, status) {
+          defer.reject(data, status);
+        });
+      return defer.promise;
+    };
+
+    service.getStreamingUsers = function getStreamingUsers(){
+      var defer = $q.defer();
+      $http.get(ServerUrl + '/users/streaming')
+        .success(function(data) {
+          defer.resolve(data);
+        })
+        .error(function(data, status) {
+          defer.reject(data, status);
+        });
+      return defer.promise;
+    };
+
+    return service;
+  });
+
+  function generateProfilePictureAddress(serverUrl, user) {
+    return user.profilePicture
+      ? serverUrl + '/pictures/' + user.id + '/small/show'
+      : '/assets/images/anonuser.png';
+  }
 })(window.angular, window.infoWindow, window.google, window.moment);
