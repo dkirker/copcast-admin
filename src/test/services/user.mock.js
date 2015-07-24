636e9dd26b8bec3f5089858ac23facfec3f8086d
Leonardo Nicolas <leonicolas@gmail.com>
2015-07-24 14:00:34 -0300

    Fix user service test

--- src/test/services/user.mock.js
+++ src/test/services/user.mock.js
@@ -4,63 +4,67 @@
 'use strict';
 
 var activeUsers = [{
-    "id": "1",
-    "username": "jonas.nord",
-    "lastLocationUpdateDate": "2015-05-19T19:45:54.754Z",
-    "name": "Jonas Nord",
-    "profilePicture": "jonas.jpg",
-    "group": "Instituto Igarapé"
+    id: '1',
+    username: 'alex',
+    lastLocationUpdateDate: '2015-05-19T19:45:54.754Z',
+    name: 'alex',
+    profilePicture: 'alex.jpg',
+    group: 'Instituto Igarapé'
 }, {
-    "id": "2",
-    "username": "alex.silva",
-    "lastLocationUpdateDate": null,
-    "name": "Alexandre Silva",
-    "profilePicture": null,
-    "group": "Metropolitan Police"
+    id: '2',
+    username: 'asalgado',
+    lastLocationUpdateDate: '2015-07-14T18:13:13.947Z',
+    name: 'alex salgado',
+    profilePicture: 'alex.jpg',
+    group: 'Instituto Igarapé'
 }];
 
 var usersBase = [{
-   "id": "1",
-   "username": "jonas.nord",
-   "passwordHash": "dwefcrefrefrwf432rfft4h4wx",
-   "passwordSalt": "d32rf4ft35d43tdxtws34trxr",
-   "name": "Jonas Nord",
-   "profilePicture": "jonas.jpg",
-   "email": "jonas.nord@test.com",
-   "gcmRegistration": "djhdhdw88998u88",
-   "isAdmin": false,
-   "lastLat": 38.7292158,
-   "lastLng": -9.1298413,
-   "lastLocationUpdateDate": "2015-05-29T01:42:49.142Z",
-   "createdAt": "2015-04-27T14:38:47.377Z",
-   "updatedAt": "2015-05-29T01:42:49.149Z",
-   "groupId": 10
+    id: '1',
+    username: 'jonas.nord',
+    passwordHash: 'dwefcrefrefrwf432rfft4h4wx',
+    passwordSalt: 'd32rf4ft35d43tdxtws34trxr',
+    name: 'Jonas Nord',
+    profilePicture: 'jonas.jpg',
+    email: 'jonas.nord@test.com',
+    gcmRegistration: 'djhdhdw88998u88',
+    isAdmin: false,
+    lastLat: 38.7292158,
+    lastLng: -9.1298413,
+    lastLocationUpdateDate: '2015-05-29T01:42:49.142Z',
+    createdAt: '2015-04-27T14:38:47.377Z',
+    updatedAt: '2015-05-29T01:42:49.149Z',
+    groupId: 10
 }, {
-   "id": "2",
-   "username": "alex.silva",
-   "passwordHash": "e2djd8diewjdoiwjndowidewddw",
-   "passwordSalt": "ewjde8wjd8ewed98uew98ue98dw",
-   "name": "Alexandre Silva",
-   "profilePicture": null,
-   "email": "alex.silve@test.com",
-   "gcmRegistration": "djhdhdw88998u88",
-   "isAdmin": true,
-   "lastLat": -22.9018647,
-   "lastLng": -43.1036538,
-   "lastLocationUpdateDate": "2015-05-29T01:42:49.142Z",
-   "createdAt": "2015-04-27T14:38:47.377Z",
-   "updatedAt": "2015-05-29T01:42:49.149Z",
-   "groupId": 10
+    id: '2',
+    username: 'alex.silva',
+    passwordHash: 'e2djd8diewjdoiwjndowidewddw',
+    passwordSalt: 'ewjde8wjd8ewed98uew98ue98dw',
+    name: 'Alexandre Silva',
+    profilePicture: null,
+    email: 'alex.silve@test.com',
+    gcmRegistration: 'djhdhdw88998u88',
+    isAdmin: true,
+    lastLat: -22.9018647,
+    lastLng: -43.1036538,
+    lastLocationUpdateDate: '2015-05-29T01:42:49.142Z',
+    createdAt: '2015-04-27T14:38:47.377Z',
+    updatedAt: '2015-05-29T01:42:49.149Z',
+    groupId: 10
 }];
 
 var user1Videos = [{
-    'id': 'v3933'
+    id: 'v3933',
+    userId: '1'
 }, {
-    'id': 'v5394'
+    id: 'v5394',
+    userId: '1'
 }];
 
 var user2Videos = [{
-    'id': 'v6545'
+    id: 'v6545',
+    userId: '2'
 }, {
-    'id': 'v0367'
+    id: 'v0367',
+    userId: '2'
 }];
