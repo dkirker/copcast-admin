adf8c8c0ac92f078ee9ad8ec57fc002c30baf28a
brunosiqueira <bsiqueira@gmail.com>
2015-10-28 16:40:32 +0000

    correcting filter #158

--- src/app/report/report.controller.js
+++ src/app/report/report.controller.js
@@ -7,17 +7,47 @@
   angular.module('copcastAdminApp').controller('ReportCtrl', ReportCtrl);
 
   function ReportCtrl($scope) {
-    $scope.filter = {fromDate: null, toDate: null}
-    $scope.reportData = {
-      activeOfficers: null,
-      missions: null,
-      recordingLength: null,
-      recordedHours: null,
-      streamedHours: null,
-      incidents: null,
-      adminAccess: null,
-      incidentReport: []
+    $scope.filter = {
+      fromDate: null,
+      toDate: null,
+      fromDateVisible: false,
+      toDateVisible: false,
+      fromDateShow: function(){
+        $scope.filter.fromDateVisible = true;
+      },
+      toDateShow: function(){
+        $scope.filter.toDateVisible = true;
+      }
+
     };
+    $scope.reportData = {
+      activeOfficers: 0,
+      missions: 1,
+      recordedHours: '15:24',
+      streamedHours: '3:54',
+      incidents: 6,
+      adminAccess: 5,
+      incidentReport: [1,4,2,6,0],
+      users: [
+        {userName: 'Bruno Siqueira',
+        activities: [
+          {
+            date: '2015-10-07',
+            recordingTime: '08:25',
+            streamingTime: '00:25',
+            pausedTime: '00:10',
+            numberMissionStopped: 4
+          }
+        ]}
+      ]
+    };
+
+    $scope.hasData = hasData;
+
+
+    function hasData(){
+      return true;
+    }
 
   }
 })(window.angular, window.moment);
