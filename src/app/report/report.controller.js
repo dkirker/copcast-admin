3b6e092e54f563b703d10a81679c8f20ef13ad06
brunosiqueira <bsiqueira@gmail.com>
2015-11-04 00:55:21 +0000

    correcting logic for exibition of data #158

--- src/app/report/report.controller.js
+++ src/app/report/report.controller.js
@@ -6,7 +6,7 @@
 
   angular.module('copcastAdminApp').controller('ReportCtrl', ReportCtrl);
 
-  function ReportCtrl($scope) {
+  function ReportCtrl($scope, $http, ServerUrl) {
     $scope.filter = {
       fromDate: null,
       toDate: null,
@@ -20,34 +20,42 @@
       }
 
     };
-    $scope.reportData = {
-      activeOfficers: 0,
-      missions: 1,
-      recordedHours: '15:24',
-      streamedHours: '3:54',
-      incidents: 6,
-      adminAccess: 5,
-      incidentReport: [1,4,2,6,0],
-      users: [
-        {userName: 'Bruno Siqueira',
-        activities: [
-          {
-            date: '2015-10-07',
-            recordingTime: '08:25',
-            streamingTime: '00:25',
-            total: '18:00',
-            pausedTime: '00:10',
-            numberMissionStopped: 4
-          }
-        ]}
-      ]
-    };
-
     $scope.hasData = hasData;
+    $scope.updateFilter = updateFilter;
 
+    //$scope.reportData = {
+    //  activeOfficers: 0,
+    //  missions: 1,
+    //  recordedHours: '15:24',
+    //  streamedHours: '3:54',
+    //  incidents: 6,
+    //  adminAccess: 5,
+    //  incidentReport: [1,4,2,6,0],
+    //  users: [
+    //    {userName: 'Bruno Siqueira',
+    //    activities: [
+    //      {
+    //        date: '2015-10-07',
+    //        recordingTime: '08:25',
+    //        streamingTime: '00:25',
+    //        total: '18:00',
+    //        pausedTime: '00:10',
+    //        numberMissionStopped: 4
+    //      }
+    //    ]}
+    //  ]
+    //};
+
+
+
+    function updateFilter(){
+      $http.get(ServerUrl + "/report/use/"+moment($scope.filter.fromDate).format('YYYY-MM-DD')+"/"+moment($scope.filter.toDate).format('YYYY-MM-DD')).success(function(data) {
+        $scope.reportData = data;
+      });
+    }
 
     function hasData(){
-      return true;
+      return $scope.reportData && $scope.reportData["activeOfficers"] !== undefined;
     }
 
   }
