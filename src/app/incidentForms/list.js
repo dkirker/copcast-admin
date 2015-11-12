c021c635d6d00f6015bf2c7813f098de6b9d73aa
salgado <alexsalgado1@gmail.com>
2015-11-12 11:48:31 -0200

    issue #95 - include data range search

--- src/app/incidentForms/list.js
+++ src/app/incidentForms/list.js
@@ -8,10 +8,28 @@
  * Controller of the copcastAdminApp
  */
 angular.module('copcastAdminApp')
-  .controller('IncidentsListCtrl', function ($scope, $http, $location, ServerUrl) {
+  .controller('IncidentsListCtrl', function ($scope, $http, $location, ServerUrl, gettextCatalog) {
 
     $scope.sortKey = "date";
     $scope.reverse = 1;
+
+    //filter to catch data range behavior
+    $scope.filter = {
+      fromDate: null,
+      toDate: null,
+      fromDateVisible: false,
+      toDateVisible: false,
+      fromDateShow: function(){
+        $scope.filter.fromDateVisible = true;
+      },
+      toDateShow: function(){
+        $scope.filter.toDateVisible = true;
+      }
+
+    };
+    $scope.hasData = function hasData(){
+      return $scope.incidentForms && $scope.incidentForms["activeOfficers"] !== undefined;
+    }
 
     //sort list
     $scope.sort = function(keyname){
@@ -20,7 +38,22 @@ angular.module('copcastAdminApp')
 
     };
 
-    // callback for ng-click 'editIncident':
+    //sort range date
+    $scope.searchByDate = function updateFilter(){
+      var fromDate = moment($scope.filter.fromDate);
+      var toDate = moment($scope.filter.toDate);
+      if (fromDate.isValid() && toDate.isValid()) {
+        $scope.errorMessage = null;
+        $http.get(ServerUrl + "/incidentForms/" + fromDate.format('YYYY-MM-DD') + "/" + toDate.format('YYYY-MM-DD'))
+          .success(function (data) {
+          $scope.incidentForms = data;
+        });
+      } else {
+        $scope.errorMessage = gettextCatalog.getString('Invalid data range.')
+      }
+    }
+
+    // callback for ng-click 'viewIncidentForm':
     $scope.viewIncident = function (incidentId) {
       $location.path('/incidentForm-view/' + incidentId);
     };
