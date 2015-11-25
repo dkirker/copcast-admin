7c34dafbc718e4003dc9d7c6f05fc860539c24f3
salgado <alexsalgado1@gmail.com>
2015-11-25 12:09:21 -0200

    Log audit report #164 - include logReport

---
+++ src/app/logReport/view.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name copcastAdminApp.controller:IncidentsViewCtrl
+ * @description
+ * # IncidentsViewCtrl
+ * Controller of the copcastAdminApp
+ */
+angular.module('copcastAdminApp')
+  .controller('IncidentsViewCtrl', function ($scope, $routeParams, $http, $location, ServerUrl){
+
+    // callback for ng-click 'back':
+    $scope.back = function () {
+      $location.path('/incidentForm-list');
+    };
+
+    //get incidentForm by id
+    $http.get(ServerUrl + '/incidentForm/'+ $routeParams.id).success(function(data) {
+      $scope.incidentForm = data;
+
+    }).error(function(data) {
+      console.log("Error: " + data);
+    });
+
+  });
