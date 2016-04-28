2197b37bac08087f29c16e17f91250ad0925c6b7
Arthur Vasconcelos <vasconcelos.arthur@gmail.com>
2016-04-28 13:00:34 -0300

    igarape/copcast-server#63 Check for null values in errors object when creating group

--- src/app/groups/creation.js
+++ src/app/groups/creation.js
@@ -10,12 +10,16 @@
 angular.module('copcastAdminApp')
   .controller('GroupsCreationCtrl', function($scope, $http, $location, ServerUrl){
     $scope.createGroup = function () {
-      $http.post(ServerUrl + '/groups',
-        $scope.group).success(function(data) {
-          $location.path('/group-list');
-        }).error(function(data) {
+      $http.post(ServerUrl + '/groups', $scope.group)
+      .success(function(data) {
+        $location.path('/group-list');
+      }).error(function(data) {
+        if (data && data.errors){
+          $scope.serverMessage = data.errors[0].message;
+        } else {
           $scope.serverMessage = data;
-        });
+        }
+      });
     };
 
     $scope.back = function () {
