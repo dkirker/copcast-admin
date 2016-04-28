4b8be5d3f0ceaeb94a6de9418376f779332444bd
brunosiqueira <bsiqueira@gmail.com>
2016-04-28 15:00:22 +0100

    #231 0 bytes sent and Incidents Flagged label

--- src/app/report/bytes.filter.js
+++ src/app/report/bytes.filter.js
@@ -7,7 +7,7 @@
 
   angular.module('copcastAdminApp').filter('bytes', function() {
     return function(bytes, precision) {
-      if (isNaN(parseFloat(bytes)) || !isFinite(bytes)) return '-';
+      if (isNaN(parseFloat(bytes)) || !isFinite(bytes) || parseFloat(bytes) == 0) return '-';
       if (typeof precision === 'undefined') precision = 1;
       var units = ['bytes', 'kB', 'MB', 'GB', 'TB', 'PB'],
         number = Math.floor(Math.log(bytes) / Math.log(1024));
