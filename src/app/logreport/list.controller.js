1786a885d41b72a26622ec59b14ac2427e50db0d
Arthur Vasconcelos <vasconcelos.arthur@gmail.com>
2016-05-10 17:08:05 -0300

    #245 Fixing JS Hint bugs in file src/app/logreport/list.controller.js

--- src/app/logreport/list.controller.js
+++ src/app/logreport/list.controller.js
@@ -8,13 +8,27 @@
  * Controller of the copcastAdminApp
  */
 angular.module('copcastAdminApp')
-  .controller('LogReportListCtrl', function ($scope, $http, $location, ServerUrl, historyService,userService, groupService, gettextCatalog) {
+  .controller('LogReportListCtrl', function ($scope, $window, $http, $location, ServerUrl, historyService,userService, groupService, gettextCatalog) {
 
-    $scope.sortKey = "date";
+    $scope.sortKey = 'date';
     $scope.reverse = 1;
     $scope.perPage = 30;
     $scope.totalHistories = 0;
     $scope.pagination = 1;
+
+    function loadHistory(){
+      historyService.listHistories($scope.filter.user, $scope.filter.group, $scope.pagination, $scope.perPage).
+      then(function(data){
+        $scope.logreports = data.rows;
+        for (var i = 0; i < $scope.logreports.length; i++){
+          var logReport = $scope.logreports[i];
+          logReport.extraJson = JSON.parse(logReport.extras);
+        }
+        $scope.totalHistories = data.count;
+      }, function(error){
+        $window.console.error(error);
+      });
+    }
 
     $scope.pageChanged = function(newPage) {
       $scope.pagination = newPage;
@@ -41,13 +55,13 @@ angular.module('copcastAdminApp')
 
     };
     $scope.hasData = function hasData(){
-      return $scope.logreport && $scope.logreport["activeOfficers"] !== undefined;
+      return $scope.logreport && $scope.logreport.activeOfficers !== undefined;
     };
 
     //sort range date
     $scope.searchByDate = function updateFilter(){
-      var fromDate = moment($scope.filter.fromDate);
-      var toDate = moment($scope.filter.toDate);
+      var fromDate = $window.moment($scope.filter.fromDate);
+      var toDate = $window.moment($scope.filter.toDate);
       if (fromDate.isValid() && toDate.isValid()) {
         $scope.errorMessage = null;
         historyService.listHistoriesByParams(fromDate, toDate, $scope.filter.user, $scope.filter.group,
@@ -56,10 +70,10 @@ angular.module('copcastAdminApp')
           $scope.logreports = data.rows;
           $scope.totalHistories = data.count;
         }, function(error){
-          console.error(error);
+          $window.console.error(error);
         });
       } else {
-        $scope.errorMessage = gettextCatalog.getString('Invalid date range.')
+        $scope.errorMessage = gettextCatalog.getString('Invalid date range.');
       }
     };
 
@@ -72,24 +86,10 @@ angular.module('copcastAdminApp')
 
     $scope.groups = [];
     groupService.listGroups().then(function(groups){
-      $scope.groups = groups
+      $scope.groups = groups;
     }, function(err){
       $scope.errorMessage = err;
     });
-
-    function loadHistory(){
-      historyService.listHistories($scope.filter.user, $scope.filter.group, $scope.pagination, $scope.perPage).
-      then(function(data){
-        $scope.logreports = data.rows;
-        for (var i = 0; i < $scope.logreports.length; i++){
-          var logReport = $scope.logreports[i];
-          logReport.extraJson = JSON.parse(logReport.extras);
-        }
-        $scope.totalHistories = data.count;
-      }, function(error){
-        console.error(error);
-      });
-    }
 
     loadHistory();
   });
