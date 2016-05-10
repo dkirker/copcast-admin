15d6a5c61b7122ee4b542ca7de1e0fcde56f1b93
Arthur Vasconcelos <vasconcelos.arthur@gmail.com>
2016-05-10 17:07:01 -0300

    #245 Fixing JS Hint bugs in file src/app/groups/list.js

--- src/app/groups/list.js
+++ src/app/groups/list.js
@@ -8,18 +8,13 @@
  * Controller of the copcastAdminApp
  */
 angular.module('copcastAdminApp')
-  .controller('GroupsListCtrl', function ($scope, $http, $location, ServerUrl) {
+  .controller('GroupsListCtrl', function ($scope, $window, $http, $location, ServerUrl) {
     $scope.page = 1;
     $scope.perPage = 30;
     $scope.totalGroups = 0;
 
     $scope.groups = [];
     $scope.isAdminGroup = false;
-
-    $scope.pageChanged = function(newPage) {
-      $scope.page = newPage;
-      loadGroups();
-    };
 
     function loadGroups(){
       var params = {
@@ -33,16 +28,21 @@ angular.module('copcastAdminApp')
         $scope.groups = data.rows;
         $scope.totalGroups = data.count;
       }).error(function(data) {
-        console.error(data);
+        $window.console.error(data);
       });
     }
+
+    $scope.pageChanged = function(newPage) {
+      $scope.page = newPage;
+      loadGroups();
+    };
 
     loadGroups();
 
     $http.get(ServerUrl + '/groups/isadmin').success(function(data) {
       $scope.isAdminGroup = data;
     }).error(function(data) {
-      console.error(data);
+      $window.console.error(data);
     });
 
     // callback for ng-click 'editGroup':
@@ -51,7 +51,7 @@ angular.module('copcastAdminApp')
     };
 
     // callback for ng-click 'deleteGroup':
-    $scope.deleteGroup = function (groupId, groupName) {
+    $scope.deleteGroup = function (groupId) {
       // confirmation to delete
       $location.path('/group-destroy/' + groupId);
     };
@@ -63,7 +63,7 @@ angular.module('copcastAdminApp')
   }).controller('GroupCreationCtrl', function($scope, $http, $location, ServerUrl){
     $scope.createGroup = function () {
       $http.post(ServerUrl + '/groups',
-        $scope.group).success(function(data) {
+        $scope.group).success(function() {
           $location.path('/group-list');
         }).error(function(data) {
           $scope.serverMessage = data;
