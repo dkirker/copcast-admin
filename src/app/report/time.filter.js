832fde67928e5fc1ccb3d3579e9e7950a7e832cf
Arthur Vasconcelos <vasconcelos.arthur@gmail.com>
2016-05-12 15:12:22 -0300

    #245 Fixing JS Hint bugs in file src/app/report/time.filter.js

--- src/app/report/time.filter.js
+++ src/app/report/time.filter.js
@@ -1,24 +1,19 @@
 /**
  * Created by brunosiqueira on 04/11/15.
  */
-(function(angular, moment) {
-  'use strict';
 
-  angular.module('copcastAdminApp').filter('minutesToHours', minutesToHours);
+'use strict';
 
-  function minutesToHours(){
-    return function(minutes){
-      if (minutes == undefined || minutes == 0){
-        return "00:00"
-      }
-
-      var hours = Math.floor(parseInt(minutes) / 60).toString();
-      var min = parseInt(minutes) - hours * 60;
-
-      return (hours.length > 1 ? hours : "0" + hours ) + ':'+("0" + min).slice(-2);
-
+angular.module('copcastAdminApp').filter('minutesToHours', function(){
+  return function(minutes){
+    if (minutes === undefined || minutes === 0){
+      return '00:00';
     }
 
-  }
-})(window.angular, window.moment);
+    var hours = Math.floor(parseInt(minutes) / 60).toString();
+    var min = parseInt(minutes) - hours * 60;
+
+    return (hours.length > 1 ? hours : '0' + hours) + ':' + ('0' + min).slice(-2);
+  };
+});
 
