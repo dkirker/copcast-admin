1540186780f23384813de7c03729768e965c90b6
Arthur Vasconcelos <vasconcelos.arthur@gmail.com>
2016-05-12 15:12:10 -0300

    #245 Fixing JS Hint bugs in file src/app/report/bytes.filter.js

--- src/app/report/bytes.filter.js
+++ src/app/report/bytes.filter.js
@@ -1,17 +1,15 @@
 'use strict';
+
 /**
  * Created by brunosiqueira on 12/04/16.
  */
-(function(angular) {
-  'use strict';
 
-  angular.module('copcastAdminApp').filter('bytes', function() {
-    return function(bytes, precision) {
-      if (isNaN(parseFloat(bytes)) || !isFinite(bytes) || parseFloat(bytes) == 0) return '-';
-      if (typeof precision === 'undefined') precision = 1;
-      var units = ['bytes', 'kB', 'MB', 'GB', 'TB', 'PB'],
-        number = Math.floor(Math.log(bytes) / Math.log(1024));
-      return (bytes / Math.pow(1024, Math.floor(number))).toFixed(precision) +  ' ' + units[number];
-    }
-  });
-})(window.angular);
+angular.module('copcastAdminApp').filter('bytes', function() {
+  return function(bytes, precision) {
+    if (isNaN(parseFloat(bytes)) || !isFinite(bytes) || parseFloat(bytes) === 0) { return '-'; }
+    if (typeof precision === 'undefined') { precision = 1; }
+    var units = ['bytes', 'kB', 'MB', 'GB', 'TB', 'PB']
+      , number = Math.floor(Math.log(bytes) / Math.log(1024));
+    return (bytes / Math.pow(1024, Math.floor(number))).toFixed(precision) +  ' ' + units[number];
+  };
+});
