87e7d390d58f0deb9b2ad3048a2f1a71e323875a
Arthur Vasconcelos <vasconcelos.arthur@gmail.com>
2016-05-12 15:12:56 -0300

    #245 Fixing JS Hint bugs in file src/app/services/history.service.js

--- src/app/services/history.service.js
+++ src/app/services/history.service.js
@@ -1,86 +1,85 @@
 /**
  * Created by brunosiqueira on 02/12/15.
  */
-;(function(angular, moment) {
-  'use strict';
+'use strict';
 
-  angular.module('copcastAdminApp')
-    .factory('historyService', function($q, $http, ServerUrl) {
-      var service = {};
+angular.module('copcastAdminApp')
+  .factory('historyService', function($q, $http, $window, ServerUrl) {
+    var service = {};
 
-      service.registerLoggedIn = function(){
+    service.registerLoggedIn = function(){
 
-        var defer = $q.defer();
-        $http
-          .post(ServerUrl + '/histories', {
-            previousState: 'NOT_LOGGED',
-            nextState: 'LOGGED_ADMIN',
-            date: moment().toDate()
-          })
-          .success(function(data) {
-            defer.resolve(data);
-          })
-          .error(function(data, status) {
-            defer.reject(data, status);
-          });
-        return defer.promise;
-      };
-
-      service.registerVideoPlay = function(videoName, startTime, user){
-
-        var defer = $q.defer();
-        $http
-          .post(ServerUrl + '/histories', {
-            previousState: 'LOGGED_ADMIN',
-            nextState: 'PLAYING_VIDEO',
-            extras: JSON.stringify({videoName: videoName, startTime: startTime, userId: user.id, userName: user.name}),
-            date: moment().toDate()
-          })
-          .success(function(data) {
-            defer.resolve(data);
-          })
-          .error(function(data, status) {
-            defer.reject(data, status);
-          });
-        return defer.promise;
-      };
-
-      service.listHistories = function(user, group, page, perPage){
-        var defer = $q.defer();
-        $http.get(ServerUrl + '/logreports',
-          { params : {
-            page : page,
-            perPage: perPage,
-            group: group,
-            user: user
-          }
-          }
-        ).success(function(data) {
+      var defer = $q.defer();
+      $http
+        .post(ServerUrl + '/histories', {
+          previousState: 'NOT_LOGGED',
+          nextState: 'LOGGED_ADMIN',
+          date: $window.moment().toDate()
+        })
+        .success(function(data) {
           defer.resolve(data);
-        }).error(function(data, status) {
+        })
+        .error(function(data, status) {
           defer.reject(data, status);
         });
-        return defer.promise;
-      }
+      return defer.promise;
+    };
 
-      service.listHistoriesByParams = function(fromDate, toDate,user, group, page, perPage){
-        var defer = $q.defer();
-        $http.get(ServerUrl + '/logreports/' + fromDate.format('YYYY-MM-DD') + '/' + toDate.format('YYYY-MM-DD'),
-          { params : {
-            page : page,
-            perPage: perPage,
-            group: group,
-            user: user
-          }
-          }
-        ).success(function(data) {
+    service.registerVideoPlay = function(videoName, startTime, user){
+      var defer = $q.defer();
+
+      $http
+        .post(ServerUrl + '/histories', {
+          previousState: 'LOGGED_ADMIN',
+          nextState: 'PLAYING_VIDEO',
+          extras: JSON.stringify({videoName: videoName, startTime: startTime, userId: user.id, userName: user.name}),
+          date: $window.moment().toDate()
+        })
+        .success(function(data) {
           defer.resolve(data);
-        }).error(function(data, status) {
+        })
+        .error(function(data, status) {
           defer.reject(data, status);
         });
-        return defer.promise;
-      }
 
-      return service;
-  });
-})(window.angular, window.moment);
+      return defer.promise;
+    };
+
+    service.listHistories = function(user, group, page, perPage){
+      var defer = $q.defer();
+      $http.get(ServerUrl + '/logreports',
+        { params : {
+          page : page,
+          perPage: perPage,
+          group: group,
+          user: user
+        }
+        }
+      ).success(function(data) {
+        defer.resolve(data);
+      }).error(function(data, status) {
+        defer.reject(data, status);
+      });
+      return defer.promise;
+    };
+
+    service.listHistoriesByParams = function(fromDate, toDate,user, group, page, perPage){
+      var defer = $q.defer();
+      $http.get(ServerUrl + '/logreports/' + fromDate.format('YYYY-MM-DD') + '/' + toDate.format('YYYY-MM-DD'),
+        { params : {
+          page : page,
+          perPage: perPage,
+          group: group,
+          user: user
+        }
+        }
+      ).success(function(data) {
+        defer.resolve(data);
+      }).error(function(data, status) {
+        defer.reject(data, status);
+      });
+      return defer.promise;
+    };
+
+    return service;
+});
