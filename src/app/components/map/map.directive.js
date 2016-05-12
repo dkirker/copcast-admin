97c496d07e1976078cbe0de73c2db607c415cf62
Arthur Vasconcelos <vasconcelos.arthur@gmail.com>
2016-05-12 15:09:51 -0300

    #245 Fixing JS Hint bugs in file src/app/components/map/map.directive.js

--- src/app/components/map/map.directive.js
+++ src/app/components/map/map.directive.js
@@ -1,25 +1,76 @@
-;(function(angular) {
   'use strict';
 
   var app = angular.module('copcastAdminApp');
 
-  app.directive('map', function() {
+  app.directive('map', function($window) {
     return {
       restrict: 'EA',
       scope: {
         markerLocations: '=?',
         heatmapLocations: '=?'
       },
-      link: function(scope, element, attrs) {
+      link: function(scope, element/*, attrs*/) {
         var DEFAULT_USER_ZOOM = 18;
         var markers = [];
         var heapmapLayer;
         var mapOptions = {
           zoom: 3,
-          center: new google.maps.LatLng(0, 0)
+          center: new $window.google.maps.LatLng(0, 0)
         };
 
-        var map = new google.maps.Map(element[0], mapOptions);
+        var map = new $window.google.maps.Map(element[0], mapOptions);
+
+        /*
+         * Position functions
+         */
+        function setMapPosition(position, zoom) {
+          map.setZoom(zoom || DEFAULT_USER_ZOOM);
+          map.panTo(position);
+        }
+
+        function resetMapPosition() {
+          setMapPosition(mapOptions.center, mapOptions.zoom);
+        }
+
+        /*
+         * Marker functions
+         */
+        function removeMarkers() {
+          for(var i = 0, len = markers.length; i < len; i++) {
+            markers[i].setMap(null);
+          }
+          markers = [];
+        }
+
+        function fitBounds(latLngPoints) {
+          if(!latLngPoints || latLngPoints.length === 0) {
+            resetMapPosition();
+          } else if(latLngPoints.length === 1) {
+            setMapPosition(latLngPoints[0]);
+          } else {
+            var bounds = new $window.google.maps.LatLngBounds();
+
+            for(var i = 0, len = latLngPoints.length; i < len; i++) {
+              bounds.extend(latLngPoints[i]);
+            }
+
+            map.setZoom(null);
+            map.fitBounds(bounds);
+          }
+        }
+
+        /*
+         * Heatmap functions
+         */
+        function enableHeatmap(latLngPoints) {
+          var pointsArray = new $window.google.maps.MVCArray(latLngPoints);
+          heapmapLayer = new $window.google.maps.visualization.HeatmapLayer({ data: pointsArray });
+          heapmapLayer.setMap(map);
+        }
+
+        function disableHeatmap() {
+          return heapmapLayer && heapmapLayer.setMap(null);
+        }
 
         /*
          * Watchers
@@ -37,72 +88,21 @@
           removeMarkers();
           var markerLocations = scope.markerLocations;
           var positions = [];
+
           if(markerLocations && markerLocations.length > 0) {
             for(var i = 0, len = markerLocations.length; i < len; i++) {
               var marker = markerLocations[i];
-              var makerClone = angular.copy(marker);
+              // var makerClone = angular.copy(marker);
               marker.setMap(map);
               markers.push(marker);
               positions.push(marker.position);
-            };
+            }
+
             fitBounds(positions);
           } else {
             resetMapPosition();
           }
         });
-
-        /*
-         * Marker functions
-         */
-        function removeMarkers() {
-          for(var i = 0, len = markers.length; i < len; i++) {
-            markers[i].setMap(null);
-          }
-          markers = [];
-        }
-
-        function fitBounds(latLngPoints) {
-          if(!latLngPoints || latLngPoints.length === 0) {
-            resetMapPosition();
-
-          } else if(latLngPoints.length === 1) {
-            setMapPosition(latLngPoints[0]);
-
-          } else {
-            var bounds = new google.maps.LatLngBounds();
-            for(var i = 0, len = latLngPoints.length; i < len; i++) {
-              bounds.extend(latLngPoints[i]);
-            };
-            map.setZoom(null);
-            map.fitBounds(bounds);
-          }
-        }
-
-        /*
-         * Position functions
-         */
-        function setMapPosition(position, zoom) {
-          map.setZoom(zoom || DEFAULT_USER_ZOOM);
-          map.panTo(position);
-        }
-
-        function resetMapPosition() {
-          setMapPosition(mapOptions.center, mapOptions.zoom);
-        }
-
-        /*
-         * Heatmap functions
-         */
-        function enableHeatmap(latLngPoints) {
-          var pointsArray = new google.maps.MVCArray(latLngPoints);
-          heapmapLayer = new google.maps.visualization.HeatmapLayer({ data: pointsArray });
-          heapmapLayer.setMap(map);
-        }
-
-        function disableHeatmap() {
-          heapmapLayer && heapmapLayer.setMap(null);
-        }
       }
     };
   });
-})(window.angular);
