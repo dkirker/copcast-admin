882f08e9ecad9b9c0ff627ca4eabd402854aa3d5
Arthur Vasconcelos <vasconcelos.arthur@gmail.com>
2016-05-12 15:09:27 -0300

    #245 Fixing JS Hint bugs in file src/app/components/loading/loading.directive.js

--- src/app/components/loading/loading.directive.js
+++ src/app/components/loading/loading.directive.js
@@ -1,48 +1,47 @@
-;(function(angular, $) {
-  'use strict';
+'use strict';
 
-  var app = angular.module('copcastAdminApp');
+var app = angular.module('copcastAdminApp');
 
-  app.directive('loading', function($timeout) {
-    return {
-      restrict: 'E',
-      templateUrl: 'app/components/loading/loading.html',
-      replace: true,
-      scope: {
-        users: '=ngModel',
-        openOnLoad: '=?',
-        onChangeUser: '&'
-      },
-      link: function(scope, el, attrs) {
-        initializeScope(scope);
+app.directive('loading', function($timeout) {
+  return {
+    restrict: 'E',
+    templateUrl: 'app/components/loading/loading.html',
+    replace: true,
+    scope: {
+      users: '=ngModel',
+      openOnLoad: '=?',
+      onChangeUser: '&'
+    },
+    link: function(scope, el/*, attrs*/) {
+      function initializeScope(scope) {
+          scope.activations = 0;
+          scope.timeout = null;
+      }
 
-        function initializeScope(scope) {
-            scope.activations = 0;
-            scope.timeout = null;
+      initializeScope(scope);
+
+      angular.element('document').on('start-loading', function modalStartLoading() {
+        if(scope.timeout) {
+          $timeout.cancel(scope.timeout);
+          scope.timeout = null;
         }
 
-        $(document).on('start-loading', function modalStartLoading() {
-          if(scope.timeout) {
-            $timeout.cancel(scope.timeout);
-            scope.timeout = null;
-          }
-          scope.activations++;
-          el.slideDown();
-        });
+        scope.activations++;
+        el.slideDown();
+      });
 
-        $(document).on('stop-loading', function modalStopLoading() {
-          if(scope.activations > 0) {
-            scope.activations--;
-          }
-          if(scope.activations === 0 && !scope.timeout) {
-            scope.timeout = $timeout(function modalLoadingTimeout() {
-              el.slideUp();
-              initializeScope(scope);
-            }, 1000);
-          }
-        });
+      angular.element('document').on('stop-loading', function modalStopLoading() {
+        if(scope.activations > 0) {
+          scope.activations--;
+        }
 
-      }
-    };
-  });
-})(window.angular, window.jQuery);
+        if(scope.activations === 0 && !scope.timeout) {
+          scope.timeout = $timeout(function modalLoadingTimeout() {
+            el.slideUp();
+            initializeScope(scope);
+          }, 1000);
+        }
+      });
+    }
+  };
+});
