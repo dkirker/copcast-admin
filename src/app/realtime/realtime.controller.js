ab7636a7b3b2393b4824b075e0598a77e700fdb2
Arthur Vasconcelos <vasconcelos.arthur@gmail.com>
2016-05-12 15:11:58 -0300

    #245 Fixing JS Hint bugs in file src/app/realtime/realtime.controller.js

--- src/app/realtime/realtime.controller.js
+++ src/app/realtime/realtime.controller.js
@@ -43,8 +43,144 @@ angular.module('copcastAdminApp').
       streetViewControl: false
     };
 
-    $scope.currentUsers = {
+    function filterUsers() {
+      if (!$scope.searchString) {
+        //show all users
+        angular.forEach($scope.activeUsers, function (user) {
+          //TODO add group so we can search for specific groups
+          user.marker.setMap($scope.myMap);
+          if (user.cityCircle) {
+            user.cityCircle.setMap($scope.myMap);
+          }
 
+        });
+      } else {
+        //allows filter by regex
+        angular.forEach($scope.activeUsers, function (user) {
+          //TODO add group so we can search for specific groups
+          var lUserName = user.userName;
+          var lUserLogin = user.login;
+          var reMatch = new RegExp($scope.searchString, 'gi');
+
+          if (lUserName.match(reMatch) || lUserLogin.match(reMatch)) {
+            user.marker.setMap($scope.myMap);
+            if (user.cityCircle) {
+              user.cityCircle.setMap($scope.myMap);
+            }
+          } else {
+            user.marker.setMap(null);
+            if (user.cityCircle) {
+              user.cityCircle.setMap(null);
+            }
+          }
+        });
+      }
+    }
+
+    function showUser(userId) {
+      $scope.currentUser = $scope.getCurrentUsers().getUser(userId);
+      $scope.streamButtonText = 'Begin stream';
+      $scope.waitingStreaming = false;
+
+      if ( $scope.currentUser ) {
+        google.maps.event.trigger($scope.myMap, 'resize');
+        $scope.myMap.setCenter($scope.currentUser.marker.getPosition());
+
+        mapService.showBalloon($scope);
+      }
+    }
+
+    function requestStream(user) {
+      socket.emit('watch', user.id);
+
+      $scope.popModal(user);
+      $window.console.log('watch: '+user.id);
+
+      //$scope.waitingStreaming = true;
+      //if ($scope.isStreaming(user)){
+      //  $scope.streamButtonText = 'streaming';
+      //showModal($scope.activeUsers[user.id]);
+      //} else {
+      //  $scope.streamButtonText = 'Sending...';
+      //}
+    }
+
+    function changeMapPos(lat, lng) {
+      var pos = new google.maps.LatLng(lat, lng);
+      $scope.myMap.panTo(pos);
+      $scope.defaultPos = pos;
+    }
+
+    function refreshMap() {
+      userService.getMyData().
+      then(function (data) {
+        if (data.length === 0) {
+          return;
+        }
+
+        if (data.lastPos && !isNaN(data.lastPos.lat) && !isNaN(data.lastPos.lng)) {
+          changeMapPos(data.lastPos.lat, data.lastPos.lng);
+        } else if (data.group.lat && data.group.lng && !isNaN(data.group.lat) && !isNaN(data.group.lat)) {
+          changeMapPos(data.group.lat, data.group.lng);
+        } else {
+          changeMapPos(0, 0);
+        }
+      });
+    }
+
+    function refreshUsers() {
+      var $faLoader = angular.element('#fetchUsers');
+      $faLoader.addClass('active');
+      $scope.refreshMap();
+
+      $window.setTimeout(function() {
+        $faLoader.removeClass('active');
+      }, 1000);
+
+      return null;
+
+      // userService.getOnlineUsers().then(function (data) {
+      //   $window.console.log('getOnlineUsers');
+      //   $window.console.log(data);
+      //
+      //   var bounds = new google.maps.LatLngBounds();
+      //
+      //   angular.forEach(data, function (user) {
+      //     //$scope.loadUser(user);
+      //     var coord = new google.maps.LatLng(user.location.lat, user.location.lng);
+      //     bounds.extend(coord);
+      //   });
+      //
+      //   $scope.myMap.fitBounds(bounds);
+      //
+      //   userService.getStreamingUsers().then(function (data) {
+      //     angular.forEach(data, function (user) {
+      //       showStream($scope.activeUsers[user.id]);
+      //     });
+      //
+      //   });
+      // });
+    }
+
+    function showModal(user) {
+      mapService.closeBalloon();
+      $window.console.log('showModal with user=[' + user.id + ']');
+      //showStream(user);
+      $scope.$uibModalInstance = $uibModal.open({
+        templateUrl: 'app/realtime/videoStream/player_h264.html',
+        controller: 'ModalVideoCtrl',
+        windowClass: 'modal-stream',
+        backdrop: false,
+        scope: $rootScope,
+        resolve: {
+          user: function () {
+            return (user);
+          }
+        }
+      });
+    }
+
+    $scope.currentUsers = {
       userDict: {},
 
       reset: function() {
@@ -55,7 +191,6 @@ angular.module('copcastAdminApp').
       },
 
       enterUser: function(userId) {
-
         if (userId in this.userDict) {
           this.exitUser(userId);
         }
@@ -87,23 +222,24 @@ angular.module('copcastAdminApp').
       },
 
       updateUser: function(userData) {
-
         $window.console.log('update user: '+userData.id);
 
-        if (!(userData.id in this.userDict))
+        if (!(userData.id in this.userDict)) {
           return null;
+        }
 
-        var tmp = jQuery.extend({}, userData);
+        var tmp = angular.extend({}, userData);
         tmp.state = 1;
         this.userDict[userData.id] = tmp;
         return userData;
       },
 
       getUser: function(userId) {
-        if (userId in this.userDict)
+        if (userId in this.userDict) {
           return this.userDict[userId];
-        else
+        } else {
           return null;
+        }
       },
 
       getAllUsers : function() {
@@ -111,12 +247,13 @@ angular.module('copcastAdminApp').
       }
     };
 
-    if ($scope.getCurrentUsers == undefined)
+    if ($scope.getCurrentUsers === undefined) {
       $scope.getCurrentUsers = function() {
 
         // $window.console.warn($scope.currentUsers.userDict);
         return $scope.currentUsers;
       };
+    }
 
     $scope.filterUsers = filterUsers;
 
@@ -132,7 +269,7 @@ angular.module('copcastAdminApp').
       notify({
         templateUrl: 'app/views/notifications/errorNotification.html',
         message: username + ' ' + gettextCatalog.getString('has flagged an incident'),
-        position: "right"
+        position: 'right'
       });
     };
 
@@ -163,25 +300,25 @@ angular.module('copcastAdminApp').
 
       var user = $scope.getCurrentUsers().getUser(data.id);
 
-      if (user == null) { //user not in list
+      if (user === null) { //user not in list
 
         // user not connected via socket. Ignoring
         $window.console.log('ignored location data');
 
       } else {
 
-        user = jQuery.extend({}, user);
+        user = angular.extend({}, user);
 
-        $window.console.log("Socket: Location received for: " + data.username + " @ " + data.location.lat + "," + data.location.lng);
+        $window.console.log('Socket: Location received for: ' + data.username + ' @ ' + data.location.lat + ',' + data.location.lng);
 
         var pos = new google.maps.LatLng(data.location.lat, data.location.lng);
-        var isNew = (user.state == 0);
+        var isNew = (user.state === 0);
 
         if (isNew) {
 
           var marker = mapService.createMarker($scope, pos, data);
 
-          var userPicture = "/assets/images/anonuser.png";
+          var userPicture = '/assets/images/anonuser.png';
 
           if (data.profilePicture) {
             userPicture = [ServerUrl, 'pictures', data.id, 'small', 'show'].join('/');
@@ -221,27 +358,28 @@ angular.module('copcastAdminApp').
       $window.console.log($scope.getCurrentUsers().userDict);
       $scope.getCurrentUsers().reset();
       $window.console.log(data);
+
       data.broadcasters.forEach(function (e) {
         $scope.getCurrentUsers().enterUser(e);
       });
 
-      $window.console.log("=====");
+      $window.console.log('=====');
       $window.console.log($scope.getCurrentUsers().userDict);
     };
 
     var prepareSocket = function(socket) {
-      $window.console.log("PREPARING !!!! <<<<<<<<<<-----");
+      $window.console.log('PREPARING !!!! <<<<<<<<<<-----');
 
       socket.emit('getBroadcasters', receiveBroadcastersList);
 
       socket.on('frame', function(frame){
-        var imgArray = new Uint8Array(frame.frame);
+        var imgArray = new $window.Uint8Array(frame.frame);
         //$window.console.log(imgArray.length);
-        $rootScope.$emit("h264Frame", imgArray);
+        $rootScope.$emit('h264Frame', imgArray);
       });
 
       socket.on('streamStopped', function(){
-        $rootScope.$emit("streamStopped");
+        $rootScope.$emit('streamStopped');
       });
 
       socket.on('userLeft', function(data) {
@@ -254,7 +392,7 @@ angular.module('copcastAdminApp').
 
         //timeoutUser($scope.activeUsers[data.userLeft]);
         mapService.closeBalloon();
-        if ($scope.$uibModalInstance != null) {
+        if ($scope.$uibModalInstance !== null) {
           $scope.$uibModalInstance.close();
           $scope.$uibModalInstance = null;
         }
@@ -279,14 +417,14 @@ angular.module('copcastAdminApp').
         loadUser(data);
       });
 
-      socket.on('disconnect', function (socket) {
+      socket.on('disconnect', function (/*socket*/) {
 
         $scope.getCurrentUsers().reset();
         $window.console.log('Got disconnect!');
         angular.element('#realtimeMapConnectionBar').fadeIn();
       });
 
-      socket.on('reconnect', function (socket) {
+      socket.on('reconnect', function (/*socket*/) {
         $window.console.log('Got reconnected!');
         angular.element('#realtimeMapConnectionBar').fadeOut();
       });
@@ -297,158 +435,19 @@ angular.module('copcastAdminApp').
       });
     };
 
-    function filterUsers() {
-
-      if (!$scope.searchString) {
-        //show all users
-        angular.forEach($scope.activeUsers, function (user) {
-          //TODO add group so we can search for specific groups
-          user.marker.setMap($scope.myMap);
-          if (user.cityCircle) {
-            user.cityCircle.setMap($scope.myMap);
-          }
-
-        });
-      } else {
-        //allows filter by regex
-        angular.forEach($scope.activeUsers, function (user) {
-          //TODO add group so we can search for specific groups
-          var lUserName = user.userName;
-          var lUserLogin = user.login;
-          var reMatch = new RegExp($scope.searchString, 'gi');
-
-          if (lUserName.match(reMatch) || lUserLogin.match(reMatch)) {
-            user.marker.setMap($scope.myMap);
-            if (user.cityCircle) {
-              user.cityCircle.setMap($scope.myMap);
-            }
-          } else {
-            user.marker.setMap(null);
-            if (user.cityCircle) {
-              user.cityCircle.setMap(null);
-            }
-          }
-        });
-      }
-    }
-
-    function showUser(userId) {
-      $scope.currentUser = $scope.getCurrentUsers().getUser(userId);
-      $scope.streamButtonText = 'Begin stream';
-      $scope.waitingStreaming = false;
-      if ( $scope.currentUser ) {
-        google.maps.event.trigger($scope.myMap, "resize");
-        $scope.myMap.setCenter($scope.currentUser.marker.getPosition());
-
-        mapService.showBalloon($scope);
-      }
-    }
-
-    function requestStream(user) {
-
-      socket.emit('watch', user.id);
-
-      $scope.popModal(user);
-
-      $window.console.log('watch: '+user.id);
-
-      //$scope.waitingStreaming = true;
-      //if ($scope.isStreaming(user)){
-      //  $scope.streamButtonText = 'streaming';
-      //showModal($scope.activeUsers[user.id]);
-      //} else {
-      //  $scope.streamButtonText = 'Sending...';
-      //}
-    }
-
     $scope.requestStream = requestStream;
 
     // function stopStream(user) {
     //   if (!user) {
     //     return;
     //   }
-    //   if ($scope.activeStreams[user.id] && $scope.activeStreams[user.id].modal != null) {
+    //   if ($scope.activeStreams[user.id] && $scope.activeStreams[user.id].modal !== null) {
     //     $scope.activeStreams[user.id].modal.close();
     //   }
     //   delete $scope.activeStreams[user.id];
     //   user.marker.setIcon(mapService.getRedMarker(user.userName));
     // }
     //
-
-    function refreshUsers() {
-
-      var $faLoader = angular.element('#fetchUsers');
-      $faLoader.addClass('active');
-      $scope.refreshMap();
-      $window.setTimeout(function() {
-        $faLoader.removeClass('active');
-      }, 1000);
-
-      return null;
-
-      // userService.getOnlineUsers().then(function (data) {
-      //   $window.console.log('getOnlineUsers');
-      //   $window.console.log(data);
-      //
-      //   var bounds = new google.maps.LatLngBounds();
-      //
-      //   angular.forEach(data, function (user) {
-      //     //$scope.loadUser(user);
-      //     var coord = new google.maps.LatLng(user.location.lat, user.location.lng);
-      //     bounds.extend(coord);
-      //   });
-      //
-      //   $scope.myMap.fitBounds(bounds);
-      //
-      //   userService.getStreamingUsers().then(function (data) {
-      //     angular.forEach(data, function (user) {
-      //       showStream($scope.activeUsers[user.id]);
-      //     });
-      //
-      //   });
-      // });
-    }
-
-
-    function refreshMap() {
-      userService.getMyData().
-      then(function (data) {
-        if (data.length === 0) {
-          return;
-        }
-        if (data.lastPos && !isNaN(data.lastPos.lat) && !isNaN(data.lastPos.lng)) {
-          changeMapPos(data.lastPos.lat, data.lastPos.lng);
-        } else if (data.group.lat && data.group.lng && !isNaN(data.group.lat) && !isNaN(data.group.lat)) {
-          changeMapPos(data.group.lat, data.group.lng);
-        } else {
-          changeMapPos(0, 0);
-        }
-      });
-    }
-
-    function changeMapPos(lat, lng) {
-      var pos = new google.maps.LatLng(lat, lng);
-      $scope.myMap.panTo(pos);
-      $scope.defaultPos = pos;
-    }
-
-    function showModal(user) {
-      mapService.closeBalloon();
-      $window.console.log('showModal with user=[' + user.id + ']');
-      //showStream(user);
-      $scope.$uibModalInstance = $uibModal.open({
-        templateUrl: 'app/realtime/videoStream/player_h264.html',
-        controller: 'ModalVideoCtrl',
-        windowClass: 'modal-stream',
-        backdrop: false,
-        scope: $rootScope,
-        resolve: {
-          user: function () {
-            return (user);
-          }
-        }
-      });
-    }
 
     // function showStream(user) {
     //   $scope.activeStreams[user.id] = {
