badd9665593e5ad8ca8c595dbe31919f8b11ce0d
Arthur Vasconcelos <vasconcelos.arthur@gmail.com>
2016-05-12 15:12:30 -0300

    #245 Fixing JS Hint bugs in file src/app/report/timeTotal.filter.js

--- src/app/report/timeTotal.filter.js
+++ src/app/report/timeTotal.filter.js
@@ -1,25 +1,19 @@
 /**
  * Created by brunosiqueira on 04/11/15.
  */
-(function(angular, moment) {
   'use strict';
 
-  angular.module('copcastAdminApp').filter('totalAndMinutesToHours', minutesToHours);
+angular.module('copcastAdminApp').filter('totalAndMinutesToHours', function(){
+  return function(activity){
+    var recorded = activity.recordingTime !== undefined ? parseInt(activity.recordingTime) : 0;
+    var streamed = activity.streamedTime !== undefined ? parseInt(activity.streamedTime) : 0;
+    var paused = activity.pausedTime !== undefined ? parseInt(activity.pausedTime) : 0;
+    var total = recorded + streamed + paused;
 
-  function minutesToHours(){
-    return function(activity){
-      var recorded = activity.recordingTime != undefined ? parseInt(activity.recordingTime) : 0;
-      var streamed = activity.streamedTime != undefined ? parseInt(activity.streamedTime) : 0;
-      var paused = activity.pausedTime != undefined ? parseInt(activity.pausedTime) : 0;
-      var total = recorded + streamed + paused;
+    var hours = Math.floor(parseInt(total) / 60);
+    var min = parseInt(total) - hours * 60;
 
-      var hours = Math.floor(parseInt(total) / 60);
-      var min = parseInt(total) - hours * 60;
-
-      return ("0" + hours).slice(-2) + ':'+("0" + min).slice(-2);
-
-    }
-
-  }
-})(window.angular, window.moment);
+    return ('0' + hours).slice(-2) + ':' + ('0' + min).slice(-2);
+  };
+});
 
