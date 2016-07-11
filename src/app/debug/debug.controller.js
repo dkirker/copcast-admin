2a604071f166be43218600331e5cda14eb52b58f
Leandro Martelli <martelli@epix.com.br>
2016-07-11 12:21:34 -0300

    Hidden debug route to check server connection state information

--- src/app/debug/debug.controller.js
+++ src/app/debug/debug.controller.js
@@ -1,13 +1,20 @@
 'use strict';
 
 
+
 angular.module('copcastAdminApp')
   .controller('DebugCtrl', function ($scope, $window, $rootScope, socket) {
     // $scope.user = user;
     $scope.dump = socket;
+    $scope.dumptxt = 'not yet';
+
+    
 
     var dumpCB = function(data) {
-      console.log(data.dump)
+      console.log($scope);
+
+      $scope.dumptxt = JSON.stringify(data.dump, null, '  ');
+      $scope.$apply();
     }
 
     $scope.update = function () {
